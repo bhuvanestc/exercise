02235b10026e446e70518179e9a3b4477f9b7b13
bhuvan <bhuvanapraap@gmail.com>
2023-07-11 22:27:06 +0200

    ex5 added

--- src/main/java/org/example/TimeExercise.java
+++ src/main/java/org/example/TimeExercise.java
@@ -9,13 +9,15 @@ import java.time.temporal.ChronoField;
 
 public class TimeExercise {
     public static void main(String[] args) {
+        /* ex:1*/
         LocalDate today = LocalDate.now();
         System.out.println("today : " + today);
+        /* ex:2*/
         LocalDateTime dTF = LocalDateTime.now();
         DateTimeFormatter dTForm = DateTimeFormatter.ofPattern("EEEE dd MMM ");
         String formattedDate = dTF.format(dTForm);
         System.out.println(formattedDate);
-
+        /* ex:3*/
         LocalDate lastMonday= LocalDate.of(2023, 07, 03);
         System.out.println("lastMonday = " + lastMonday);
         LocalDate lastSunday= LocalDate.of(2023, 07, 10);
@@ -24,14 +26,14 @@ public class TimeExercise {
             System.out.println(date);
         }
 
-
+        /* ex:4*/
         LocalDate str = LocalDate.parse("2023-07-12");
         System.out.println("LocalDate : " + str);
 
-
-
-
-
+        /* ex:5*/
+        LocalDate givenDate= LocalDate.of(1945, 05, 8);
+        DayOfWeek dayOfWeek = DayOfWeek.from(givenDate);
+        System.out.println("dayOfWeek = " + dayOfWeek);
 
 
 
