eb8e3b35594eab93e90b3995839b9c1a521c05e1
bhuvan <bhuvanapraap@gmail.com>
2023-08-01 22:15:56 +0200

    ex7,9,10 added

--- src/main/java/org/example/Array.java
+++ src/main/java/org/example/Array.java
@@ -99,17 +99,13 @@ public class Array {
           System.out.println("Expanded array: " + Arrays.toString(newArray));
           System.out.println("ex:10, Multiplication Table");
           int[][] multiplicationTable = new int[10][10];
-          for (int i = 0; i < 10; i++) {
-               for (int j = 0; j < 10; j++) {
-                    multiplicationTable[i][j] = (i + 1) * (j + 1);
+          for (int i = 1; i < 10; i++) {
+               for (int j = 1; j < 10; j++) {
+                    System.out.printf("%5d ", i*j);
                }
+               System.out.println(" ");
           }
-          for (int i = 0; i < 10; i++) {
-               for (int j = 0; j < 10; j++) {
-                    System.out.print(multiplicationTable[i][j] + "\t");
-               }
-               System.out.println();
-          }
+
 
 
 
