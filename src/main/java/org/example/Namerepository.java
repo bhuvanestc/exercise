d0a0f5048cee8f7bac32d682774108c45d123cd3
bhuvan <bhuvanapraap@gmail.com>
2023-08-02 10:44:06 +0200

    added set names, clear, find all

--- src/main/java/org/example/Namerepository.java
+++ src/main/java/org/example/Namerepository.java
@@ -9,7 +9,23 @@ public class Namerepository {
         {
             return 0;
         }
-    return names.length
+    return names.length;
+    }
+    public static void setNames(String[] newNames)
+    {
+        names = Arrays.copyOf(newNames, newNames.length);
+    }
+    public static void clear()
+    {
+        names=null;
+    }
+    public static String[] findAll()
+    {
+        if(names==null)
+        {
+            return new String[0];
+        }
+        return Arrays.copyOf(names, names.length);
     }
 
 }
