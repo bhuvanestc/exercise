e62caf1fac23ef8d7d065c371a504169466883b2
bhuvan <bhuvanapraap@gmail.com>
2023-08-02 15:35:36 +0200

    main added to check all the methods written

--- src/main/java/org/example/Namerepository.java
+++ src/main/java/org/example/Namerepository.java
@@ -1,7 +1,72 @@
 package org.example;
 import java.util.Arrays;
-
 public class Namerepository {
+    public static void main(String[] args)
+    {
+        String[] names = {"Erik Andersson","Anna Gustavsberg","Olga Johnsson","Hanna Paul"};
+        Namerepository.setNames(names);
+
+        int size = Namerepository.getSize();
+        System.out.println("Number of names:"+size);
+
+        String fullName = "Anna Gustavsberg";
+        String foundName = Namerepository.find(fullName);
+        System.out.println("Namefound :" + foundName);
+
+        String FullName_added = "Olga Johnsson";
+        boolean nametoAdd =Namerepository.add(FullName_added);
+        if (nametoAdd)
+        {
+            System.out.println(FullName_added +" added");
+        }
+        else
+        {
+            System.out.println(FullName_added +" already exists");
+        }
+
+        String firstName = "Erik";
+        String [] namesByFirstName = Namerepository.findByfirstName(firstName);
+        for (String name: namesByFirstName)
+        {
+            System.out.println("FirstName:"+name);
+        }
+
+        String lastName = "Paul";
+        String [] namesByLastName = Namerepository.findBylastName(lastName);
+        for (String name: namesByLastName)
+        {
+            System.out.println("LastName: "+name);
+        }
+
+        String givenName = "Erik Andersson";
+        String updatedName = "Erik Svensson";
+        boolean tobeUpdated = Namerepository.update(givenName, updatedName);
+        if (tobeUpdated)
+        {
+            System.out.println("Updated Name:" + updatedName);
+        }else
+        {
+            System.out.println("Unable to update = " + givenName);
+        }
+
+        String NametobeRemoved = "Olga Johnsson";
+        boolean isRemoved = Namerepository.remove(NametobeRemoved);
+        if (isRemoved)
+        {
+            System.out.println(NametobeRemoved+" is removed");
+        }
+        else
+        {
+            System.out.println(NametobeRemoved+"Could not be removed");
+        }
+
+        String[] Nameslist = Namerepository.findAll();
+        System.out.println("Names list");
+        for (String name : Nameslist)
+        {
+            System.out.println(name);
+        }
+    }
     private static String[] names;
 
     public static int getSize() {
@@ -26,7 +91,7 @@ public class Namerepository {
         return Arrays.copyOf(names, names.length);
     }
 
-    public static String find(final string fullName) {
+    public static String find(final String fullName) {
 
         if (names == null) {
             return null;
@@ -51,7 +116,7 @@ public class Namerepository {
                 return false;
             }
         }
-        String[] newNames = Arrays.copyOf(names, names.length + 1)
+        String[] newNames = Arrays.copyOf(names, names.length + 1);
         newNames[newNames.length - 1] = fullName;
         names = newNames;
         return true;
@@ -68,7 +133,7 @@ public class Namerepository {
             int count = 0;
 
             for (String name : names) {
-                if (name.stratsWith(firstName + " ")) {
+                if (name.startsWith(firstName + " ")) {
                     result[count++] = name;
                 }
             }
@@ -93,7 +158,7 @@ public class Namerepository {
             return Arrays.copyOf(result, count);
         }
     }
-    public static boolean update(final string original,final String upDatedName)
+    public static boolean update(final String original, final String upDatedName)
     {
         if (names == null)
         {
@@ -107,18 +172,18 @@ public class Namerepository {
             {
                 index=i;
             }
-            if(updatedName.equals(names[i]))
+            if(upDatedName.equals(names[i]))
             {
                 nameExists = true;
             }
         }
         if(index == -1)
         {
-            return fasle;
+            return false;
         }
-        if(nameExits)
+        if(nameExists)
         {
-            return fasle;
+            return false;
         }
         names[index]= upDatedName;
         return true;
